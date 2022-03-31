5cb9768fb274853bf321f07e5cffa0e34ad50c19
Abdullah Akbar <abdullahakbar419@gmail.com>
2022-03-31 10:08:52 +0700

    Profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,56 +12,365 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Profile',
+      /** Navigation Bar */
       home: Scaffold(
-        body: Container(
-          margin: const EdgeInsets.fromLTRB(15, 20, 15, 0),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: BackButton(
+            color: const Color.fromRGBO(72, 72, 72, 0.90),
+            onPressed: () {},
+          ),
+          title: const Text(
+            'Profil',
+            style: TextStyle(
+              color: Color.fromRGBO(159, 159, 159, 0.6),
+            ),
+          ),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.mode_edit,
+                  color: Color.fromRGBO(72, 72, 72, 0.90),
+                ))
+          ],
+        ),
+        body: Center(
           child: Column(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              /** Photo Profile */
+              Container(
+                  margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(100),
+                            topRight: Radius.circular(100),
+                            bottomLeft: Radius.circular(100),
+                            bottomRight: Radius.circular(100)),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey.withOpacity(0.6),
+                              spreadRadius: 3,
+                              blurRadius: 7,
+                              offset: const Offset(0, 5))
+                        ]),
+                    child: const CircleAvatar(
+                      backgroundImage:
+                          AssetImage('assets/images/FotoAkbar.jpeg'),
+                      radius: 50,
+                    ),
+                  )),
+              /** Identity */
+              Column(
                 children: [
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.arrow_back,
-                        color: Colors.purple.withOpacity(0.6),
-                        size: 30,
-                      ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(15, 0, 0, 0),
-                        child: Text(
-                          'profil',
-                          style: TextStyle(
-                              fontSize: 24,
-                              color: Colors.purple.withOpacity(0.6)),
-                        ),
-                      )
-                    ],
+                  const SizedBox(
+                    height: 15,
                   ),
-                  Row(children: [
-                    Icon(
-                      Icons.mode_edit,
-                      color: Colors.purple.withOpacity(0.6),
-                      size: 30,
-                    )
-                  ]),
+                  Text(
+                    'Abdullah Akbar',
+                    style: GoogleFonts.poppins(
+                        fontSize: 32, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'abdullah.065119043@unpak.ac.id',
+                    style: GoogleFonts.poppins(
+                        fontSize: 14, color: Colors.grey.withOpacity(0.7)),
+                  ),
+                  Text(
+                    '085322153320',
+                    style: GoogleFonts.poppins(
+                        fontSize: 14, color: Colors.grey.withOpacity(0.7)),
+                  )
                 ],
               ),
-              Row(
-                children: [],
+              /** Identitiy Collage Student */
+              Container(
+                margin: const EdgeInsets.fromLTRB(25, 30, 25, 0),
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                    color: const Color.fromRGBO(117, 87, 153, 1),
+                    borderRadius: BorderRadius.circular(15)),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'NPM',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Row(
+                          children: const [
+                            Text(
+                              '065119043',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Icon(
+                              Icons.copy,
+                              size: 15,
+                              color: Colors.white,
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.white,
+                              width: 0.5,
+                              style: BorderStyle.solid),
+                          color: Colors.white),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Status Keaktifan',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Row(
+                          children: const [
+                            Text(
+                              'Aktif',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.white,
+                              width: 0.5,
+                              style: BorderStyle.solid),
+                          color: Colors.white),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Program Studi',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Row(
+                          children: const [
+                            Text(
+                              'Ilmu Komputer',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.white,
+                              width: 0.5,
+                              style: BorderStyle.solid),
+                          color: Colors.white),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Jenjang Pendidikan',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Row(
+                          children: const [
+                            Text(
+                              'S1',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.white,
+                              width: 0.5,
+                              style: BorderStyle.solid),
+                          color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(25, 40, 25, 0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                          child: const Text(
+                            'Nama Lengkap',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                          child: Row(
+                            children: const [
+                              Text(
+                                'Abdullah Akbar',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: const Color.fromRGBO(117, 87, 153, 1),
+                              width: 0.5,
+                              style: BorderStyle.solid),
+                          color: Colors.white),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                          child: const Text(
+                            'Panggilan',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                          child: Row(
+                            children: const [
+                              Text(
+                                'Akbar / Abay',
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: const Color.fromRGBO(117, 87, 153, 1),
+                              width: 0.5,
+                              style: BorderStyle.solid),
+                          color: Colors.white),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                              child: const Text(
+                                'Alamat Rumah',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                              width: 360,
+                              padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                              child: const Text(
+                                'Jl. Pahlawan No:03 Kelurahan Bondongan Kota Bogor ',
+                                maxLines: 3,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: const Color.fromRGBO(117, 87, 153, 1),
+                              width: 0.5,
+                              style: BorderStyle.solid),
+                          color: Colors.white),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Kartu Mahasiswa',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          Row(
+                            children: const [
+                              Icon(
+                                Icons.badge,
+                                color: Color.fromRGBO(117, 87, 153, 1),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               )
             ],
           ),
